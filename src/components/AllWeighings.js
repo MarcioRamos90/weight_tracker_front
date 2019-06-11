146ef9bccc18ef6b72761e9d62fbcdbed876030e
Márcio Alves Ramos <marcioramos@Mac-Marcio-Ramos.local>
2019-06-10 22:15:29 -0300

    feat: Add dynamic weight information

--- src/components/AllWeighings.js
+++ src/components/AllWeighings.js
@@ -1,39 +1,45 @@
 import React from 'react'
-import { Table } from 'semantic-ui-react'
+import { Table, Grid } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { getWeights } from '../actions'
 
 class Weighings extends React.Component {
+  state = {
+    finishLoading: false
+  }
+
   componentDidMount() {
-    const { weights, getWeights } = this.props
-    getWeights()
-    console.log(weights);
+    const { getWeights } = this.props
+
+    getWeights().then(finishLoading => this.setState({finishLoading}))
   }
 
   render() {
+    const { finishLoading } = this.state
+    const { weights } = this.props
+
+    if(!finishLoading){
+      return <Grid centered>...loading</Grid>
+    }
+
     return (
-      <Table  inverted >
+      <Table inverted >
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>Data</Table.HeaderCell>
             <Table.HeaderCell>Peso</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-    
+
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>30/02/2019</Table.Cell>
-            <Table.Cell>81,9</Table.Cell>
+        {!!weights && weights.map(item => (
+          <Table.Row key={item.id}>
+            <Table.Cell>{item.date}</Table.Cell>
+            <Table.Cell>{item.weight}</Table.Cell>
           </Table.Row>
-          <Table.Row>
-            <Table.Cell>30/03/2019</Table.Cell>
-            <Table.Cell>81,5</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>30/04/2019</Table.Cell>
-            <Table.Cell>80,9</Table.Cell>
-          </Table.Row>
-        </Table.Body>
+        ))
+      }
+      </Table.Body>
       </Table>
     )
 
