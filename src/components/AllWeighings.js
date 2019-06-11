6c2580ded8d34c2b2426bf9bd82a9149c37e92bd
Márcio Alves Ramos <marcioramos@Mac-Marcio-Ramos.local>
2019-06-10 21:50:33 -0300

    feat: Connect component with action

--- src/components/AllWeighings.js
+++ src/components/AllWeighings.js
@@ -1,30 +1,52 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { getWeights } from '../actions'
 
-const Weighings = () => (
-  <Table  inverted >
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell>Data</Table.HeaderCell>
-        <Table.HeaderCell>Peso</Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
+class Weighings extends React.Component {
+  componentDidMount() {
+    const { weights, getWeights } = this.props
+    getWeights()
+    console.log(weights);
+  }
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>30/02/2019</Table.Cell>
-        <Table.Cell>81,9</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>30/03/2019</Table.Cell>
-        <Table.Cell>81,5</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>30/04/2019</Table.Cell>
-        <Table.Cell>80,9</Table.Cell>
-      </Table.Row>
-    </Table.Body>
-  </Table>
-)
+  render() {
+    return (
+      <Table  inverted >
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Data</Table.HeaderCell>
+            <Table.HeaderCell>Peso</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+    
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>30/02/2019</Table.Cell>
+            <Table.Cell>81,9</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>30/03/2019</Table.Cell>
+            <Table.Cell>81,5</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>30/04/2019</Table.Cell>
+            <Table.Cell>80,9</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    )
 
-export default Weighings
+  }
+
+}
+
+const mapDispatchToProps = dispatch => ({
+  getWeights: () => dispatch(getWeights())
+})
+
+const mapStateToProps = ({weights}) => ({
+  weights
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Weighings)
